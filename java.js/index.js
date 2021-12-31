af99f7800b7aff73e933f6b9bebd5524e63e9f37
GLAUDSON NUNES <93484378+Glaudson-Nunes@users.noreply.github.com>
2021-12-30 21:25:13 -0300

    Add files via upload

---
+++ java.js/index.js
@@ -0,0 +1,37 @@
+
+
+
+var numero = parseInt(prompt("DIGITE UM NUMERO 0 A 5"));
+
+switch(numero){
+
+    case 1:
+
+    alert("VOCE DIGITOU 1:");
+    break;
+
+    case 2:
+
+    alert("VOCE DIGITOU 2:");
+    break;
+
+    case 3:
+    
+    alert("VOCE DIGITOU 3:");
+    break;
+
+    case 4:
+
+    alert("VOCE DIGITOU 4:");
+    break;
+
+    case 5:
+
+     alert("VOCE DIGITOU 5:");
+     break;
+
+     default:
+     alert("ERRO");
+     prompt("1: VOLTAR");
+     var numero = parseInt(prompt("DIGITE UM NUMERO 0 A 5"));
+}
